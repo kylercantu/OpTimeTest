6846663e90409ccb64f2167c1521008c34c6dab8
John-Chown <91563826+John-Chown@users.noreply.github.com>
2021-11-28 17:05:28 -0500

    Update box.java

--- TestOpTime/src/testoptime/box.java
+++ TestOpTime/src/testoptime/box.java
@@ -1,147 +1,60 @@
 
-package testoptime;
+package optime;
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
+import javax.swing.table.DefaultTableModel;
+
 
 public class box extends JPanel implements ActionListener{
-    
-     private static String[] optionStrings = {"Day", "Week", "Month", "Year" };
+
+    private static String[] optionStrings = {"Day", "Week", "Month", "Year" };
     private static JComboBox optionList = new JComboBox(optionStrings);
-    
     private static String[] columnNames = {"Program Name", "Total Time", "Average Time"};
     
-    
-    
-    private static final Object[][] data = {
-        {"p1", "590 mins", "24mins"},
-        {"p2", "590 mins", "24mins"},
-        {"p3", "590 mins", "24mins"},  
-};  
-    
-    private static final Object[][] wd = {
-        {"p1", "510 mins", "21mins"},
-        {"p2", "490 mins", "29mins"},
-        {"p3", "390 mins", "25mins"},
-   
-    
-};
-    
-    box(){
+    box() throws IOException{
+           
         this.add(optionList);
         optionList.addActionListener(this);
-        
     }
 
-    @Override
+@Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==optionList){
+      if (e.getSource()==optionList){
           
           if (optionList.getSelectedItem().equals("Day")){
-              JTable day = new JTable(data, columnNames);
-              day.setBounds(80, 80, 350, 250);
-              day.setPreferredScrollableViewportSize(new Dimension(500, 50));
-              day.setFillsViewportHeight(true);
-              JFrame y = new JFrame();   
-              y.setSize(500, 500);
-              JPanel x = new JPanel();
-              x.setLayout(null);
-              y.add(x);
-              x.add(day);
-              y.setVisible(true);
-              y.setAlwaysOnTop(true);
-              y.setLocationRelativeTo(null);
-              JScrollPane scrollPane = new JScrollPane(day);
-              scrollPane.setBounds(80, 80, 350, 250);
-              x.add(scrollPane);
+              day q = new day();
               
           }
           
           if (optionList.getSelectedItem().equals("Week")){
-              JTable week = new JTable(wd, columnNames);
-              week.setBounds(80, 80, 350, 250);
-              week.setPreferredScrollableViewportSize(new Dimension(500, 50));
-              week.setFillsViewportHeight(true);
-              JFrame y = new JFrame();   
-              y.setSize(500, 500);
-              JPanel x = new JPanel();
-              x.setLayout(null);
-              y.add(x);
-              x.add(week);
-              y.setVisible(true);
-              y.setAlwaysOnTop(true);
-              y.setLocationRelativeTo(null);
-              JScrollPane scrollPane = new JScrollPane(week);
-              scrollPane.setBounds(80, 80, 350, 250);
-              x.add(scrollPane);
+              week w = new week();
               
           } 
           
           if (optionList.getSelectedItem().equals("Month")){
-              JTable month = new JTable(wd, columnNames);
-              month.setBounds(80, 80, 350, 250);
-              month.setPreferredScrollableViewportSize(new Dimension(500, 50));
-              month.setFillsViewportHeight(true);
-              JFrame y = new JFrame();   
-              y.setSize(500, 500);
-              JPanel x = new JPanel();
-              x.setLayout(null);
-              y.add(x);
-              x.add(month);
-              y.setVisible(true);
-              y.setAlwaysOnTop(true);
-              y.setLocationRelativeTo(null);
-              JScrollPane scrollPane = new JScrollPane(month);
-              scrollPane.setBounds(80, 80, 350, 250);
-              x.add(scrollPane);
+              month m = new month();
               
           } 
           
           if (optionList.getSelectedItem().equals("Year")){
-              JTable year = new JTable(wd, columnNames);
-              year.setBounds(80, 80, 350, 250);
-              year.setPreferredScrollableViewportSize(new Dimension(500, 50));
-              year.setFillsViewportHeight(true);
-              JFrame y = new JFrame();   
-              y.setSize(500, 500);
-              JPanel x = new JPanel();
-              x.setLayout(null);
-              y.add(x);
-              x.add(year);
-              y.setVisible(true);
-              y.setAlwaysOnTop(true);
-              y.setLocationRelativeTo(null);
-              JScrollPane scrollPane = new JScrollPane(year);
-              scrollPane.setBounds(80, 80, 350, 250);
-              x.add(scrollPane);
+              year y = new year();
               
-          } 
       }
-        
     }
     
-     public static void main(String[] args) throws IOException {
-        Scanner fileIn = new Scanner(new File("test.txt"));
-        
-        box na = null;
-        String proName;
-        int tt;
-        int at;
-        
-    while (fileIn.hasNext()) {
-            proName = fileIn.next();
-            tt = fileIn.nextInt();
-            at = fileIn.nextInt();
     }
-    }
-    
-}//End box class
+}
