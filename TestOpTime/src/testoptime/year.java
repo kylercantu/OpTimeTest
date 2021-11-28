6a3dfd9220bf2b0227b4f4a3d0b14b391e8ba7a2
John-Chown <91563826+John-Chown@users.noreply.github.com>
2021-11-28 17:14:45 -0500

    Update year.java

--- TestOpTime/src/testoptime/year.java
+++ TestOpTime/src/testoptime/year.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package optime;
+package testoptime;
 
 import java.awt.Dimension;
 import java.io.BufferedReader;
@@ -82,34 +82,3 @@ public final class year {
             
     }
 }
-
-    
-    
-
-  
-        
-        
-    
-    
-    
-    
-
-
-        
-    
-
-        
-           
-        
-       
-    
-
-
-
-
-    
-
-
-
-
-
